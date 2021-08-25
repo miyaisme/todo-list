7741ebc59fd612160a6294d5947929903e397a68
a2602619 <a2602619@gmail.com>
2021-08-25 17:40:57 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const todoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+})
+module.exports = mongoose.mondel('Todo', todoSchema)
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
